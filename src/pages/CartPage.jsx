022cd4c4a9c801823a98b3f686ff09824c42c358
Syam <nmsyam.mohan@gmail.com>
2025-02-02 12:23:20 +0530

    product details link in cart implemented

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { removeFromCart } from '../features/cart/cartSlice';
+import { Link } from 'react-router-dom';
 
 function CartPage() {
     const dispatch = useDispatch();
@@ -19,24 +20,27 @@ function CartPage() {
                         key={item.id}
                         className="border-b-[1.5px] border-gray-200 w-full flex flex-col md:flex-row items-center p-3 gap-4"
                     >
-                        <img
-                            src={item.images[0]}
-                            alt="Prod-Image"
-                            className="h-32 w-32 object-cover rounded-lg"
-                        />
 
+                        <Link to={`/product-details/${item.id}`}>
+                            <img
+                                src={item.images[0]}
+                                alt="Prod-Image"
+                                className="h-32 w-32 object-cover rounded-lg"
+                            />
 
+                        </Link>
                         <div className="flex flex-col w-full md:w-2/3">
-                            <p className="font-semibold md:font-normal text-lg md:text-2xl">{item.title}</p>
-                            <p className="text-sm">
-                                Rating: <span className="bg-amber-300 px-2 rounded-md">{item.rating}</span>
-                            </p>
-                            <p className="text-sm">
-                                Return Policy: <span className="text-red-600 font-semibold">{item.returnPolicy}</span>
-                            </p>
-                            <p className="text-sm">
-                                Shipping: <span className="text-blue-600 font-semibold">{item.shippingInformation}</span>
-                            </p>
+                            <Link to={`/product-details/${item.id}`}>   <p className="font-semibold md:font-normal text-lg md:text-2xl">{item.title}</p>
+                                <p className="text-sm">
+                                    Rating: <span className="bg-amber-300 px-2 rounded-md">{item.rating}</span>
+                                </p>
+                                <p className="text-sm">
+                                    Return Policy: <span className="text-red-600 font-semibold">{item.returnPolicy}</span>
+                                </p>
+                                <p className="text-sm">
+                                    Shipping: <span className="text-blue-600 font-semibold">{item.shippingInformation}</span>
+                                </p>
+                            </Link>
                         </div>
 
 
